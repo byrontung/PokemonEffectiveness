ff641eaff4b1f0cfefc04d558e7c3c3b09025f36
byrontung <btung1@ualberta.ca>
2021-01-19 16:41:49 -0700

    Setup for pathlib

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from Pokemon import *
 from menu import menu
 from pokeeffectiveness import *
 
+
+#add pathlib for path organization https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
 def main():
     # user_in = user_input()
     # while user_in != "Exit":
